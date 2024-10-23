7812466db136674a1d73609c02738ce691220eda
swapnilkuthe <swapnilkuthe20@gmail.com>
2024-10-23 23:31:53 +0530

    first commit after forcefully remove .git folder

---
+++ config.js
@@ -0,0 +1,7 @@
+console.log("config version 1");
+console.log("Update in this file to practice checkout command");
+console.log("fourth version of commit");
+console.log("fourth version of commit");
+console.log("fourth version of commit");
+console.log("fourth version of commit");
+console.log("fourth version of commit");
---
+++ src/index.js
@@ -0,0 +1,4 @@
+console.log("index file 1");
+console.log("upadate here to add in stage area");
+console.log("update to practice to go in the specific version in the history");
+console.log("fourth version of commit");
